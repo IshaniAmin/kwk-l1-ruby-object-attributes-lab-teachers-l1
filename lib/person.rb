3c13aa76927dbfeee0ab12cf392bb011cd7c63d5
Ishani <ishaniamin@gmail.com>
2018-06-19 14:55:55 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,21 +1,6 @@
 # person.rb
 
-class Dog
+class person
 
-  def breed
-    @breed
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def name
-    @name
-  end
-
-  def name=(name)
-    @name = name
-  end
 
 end
